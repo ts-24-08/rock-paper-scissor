4f67ecbeead6f9348d863fb2a09b8ab1f10922a1
stephipie <stephi.pietschmann@gmail.com>
2025-01-13 16:57:35 +0100

    add vite tailwind prittier and set classes to index.html

---
+++ rock-paper-scissor/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      metalmania: ['Metalmania', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
