c0cec25858dfa9e5974c49dc8a0852dd3132d13f
stephipie <stephi.pietschmann@gmail.com>
2025-01-16 21:14:50 +0100

    add main.js

--- rock-paper-scissor/src/main.js
+++ rock-paper-scissor/src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+import { getComputerChoice, getResults } from "./gameLogic";
+
+const startScreen = document.getElementById("startscreen");
+const startButton = document.getElementById("startbutton");
+const rulesButton = document.getElementById("rulesbutton");
+const score = document.getElementById("score");
+const results = document.getElementById("results");
+
+// Function to hide Starting Screen
+function startGame() {
+    startScreen.classList.add("hidden");
+};
+
+startButton.addEventListener("click", startGame);
