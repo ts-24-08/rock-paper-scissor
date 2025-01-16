373ef1a5dadb8f639820b5315eb3ff01886e6db7
stephipie <stephi.pietschmann@gmail.com>
2025-01-16 17:35:03 +0100

    add tailwind to result and set colors to tailwind.config

--- rock-paper-scissor/tailwind.config.js
+++ rock-paper-scissor/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "rock": "#eb9e10",
+        "scissors": "#de3b5a",
+        "paper": "#4564f3"
+      }
+    },
     fontFamily: {
       metalmania: ['Metalmania', 'sans-serif'],
     },
